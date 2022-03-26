98734e44add750e7f838130cfc14ad4f08d572dc
Gizmo-30 <anvaradizov80@gmail.com>
2022-03-26 16:43:16 +0500

    third

---
+++ app/src/script/about/about.js
@@ -0,0 +1,16 @@
+export const about = () => {
+    const block = document.querySelectorAll('.main__blockFirst'),
+        title = document.querySelectorAll('.main__blockFirst-title'),
+        img = document.querySelectorAll('img'),
+        button = document.querySelectorAll('.main__blockFirst-btn');
+
+
+    block.forEach(item => {
+        item.addEventListener('mouseover', (e) => {
+            
+        })
+    });
+
+}
+
+export default about
--- app/src/script/app.js
+++ app/src/script/app.js
@@ -1,2 +1,4 @@
 import { header } from "./header/header.js";
 header()
+import { about } from "./menuAnim/menuAnim.js";
+about()
